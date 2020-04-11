e210340143cb022420c3b007a0021a8b8fee99e9
kobeydon <pacemaking313gmail.com>
2020-04-11 17:40:02 +0900

    key read from txt

--- slackapi.py
+++ slackapi.py
@@ -4,7 +4,7 @@ class chatbot:
     def __init__(self, msg, auth):
         self.msg = msg
         self.url = 'https://slack.com/api/chat.postMessage'
-        self.headers = {'Authorization':'Bearer {}'.format(auth)}
+        self.headers = {'Authorization':'Bearer {}'.format(auth), 'Content-Type': 'application/json; charset=utf-8'}
 
     def postmsg(self):
         response = requests.post(self.url, headers=self.headers, json=self.msg)
